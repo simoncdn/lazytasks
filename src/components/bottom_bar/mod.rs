38140c53a4c0f9daa8934f435ebdef2029f1d94f
Simon Cardona <105924355+simoncdn@users.noreply.github.com>
2025-12-30 19:26:37 +0000

    feat: add bottom_bar mod (#56)
    * feat: add bottom_bar mod
    
    * fix: remove useless vec

---
+++ src/components/bottom_bar/mod.rs
@@ -0,0 +1,19 @@
+pub mod app_info;
+pub mod key_hints;
+
+use ratatui::{
+    Frame,
+    layout::{Constraint, Direction, Layout, Rect},
+};
+
+use crate::{app::App, components::bottom_bar};
+
+pub fn render(frame: &mut Frame, area: Rect, app: &mut App) {
+    let bottom_bar_layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(90), Constraint::Percentage(10)])
+        .split(area);
+
+    bottom_bar::key_hints::render(frame, bottom_bar_layout[0], app);
+    bottom_bar::app_info::render(frame, bottom_bar_layout[1]);
+}
