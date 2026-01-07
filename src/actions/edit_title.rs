be2ea2a7748901e8857914d0895f5d2d6c6a398a
Simon Cardona <simon.cdna@proton.me>
2026-01-07 09:20:24 +0100

    fix: remove unecessary to_string

--- src/actions/edit_title.rs
+++ src/actions/edit_title.rs
@@ -6,7 +6,7 @@ use crate::app::App;
 pub fn edit_title(app: &mut App, task_id: Uuid, title: String) {
     if !title.is_empty() {
         if let Some(task) = app.tasks.iter_mut().find(|task| task.id == task_id) {
-            task.title = title.to_string();
+            task.title = title;
             task.updated_at = Some(Utc::now());
         }
         app.storage.save(&app.tasks);
